d45fadbda8095af386e684ec729ded782dd3ff6a
psaboureau <psaboureau@gmx.fr>
2024-09-18 22:07:14 +0200

    first setup (html, no css, minimal js)

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", () => { console.log(searchInput.value) })
