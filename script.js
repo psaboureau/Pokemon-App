fa9503e78592c91337df779bcfd74a94d5d11212
psaboureau <psaboureau@gmx.fr>
2024-09-20 14:01:48 +0200

    project done

--- script.js
+++ script.js
@@ -14,39 +14,66 @@ const pokemonAttack = document.getElementById("attack");
 const pokemonDefense = document.getElementById("defense");
 const pokemonSpecialAttack = document.getElementById("special-attack");
 const pokemonSpecialDefense = document.getElementById("special-defense");
-const pokemonSpeed = document.getElementById("speed")
+const pokemonSpeed = document.getElementById("speed");
 
-// TODO: Add the code to display the different types with the proper colors
+const pokemonTypesColors = {
+  grass: "#3FA129",
+  poison: "#9141CB",
+  fire: "#E62829",
+  flying: "#81B9EF",
+  water: "#2980EF",
+  bug: "#91A119",
+  normal: "#9FA19F",
+  electric: "#FAC000",
+  ground: "#915121",
+  fairy: "#EF70EF",
+  fighting: "#FF8000",
+  psychic: "#EF4179",
+  rock: "#AFA981",
+  steel: "#60A1B8",
+  ice: "#3DCEF3",
+  ghost: "#3DCEF3",
+  dragon: "#5060E1",
+  dark: "#624D4E",
+};
+
 const displayScreen = (name, id, weight, height, sprite, types) => {
-
   pokemonName.textContent = name.toUpperCase();
   pokemonId.textContent = "#" + id;
   pokemonWeight.textContent = "Weight: " + weight;
   pokemonHeight.textContent = "Height: " + height;
-  pokemonSprite.classList.add("visible")
+  pokemonSprite.classList.add("visible");
   pokemonSprite.classList.remove("hidden");
   pokemonSprite.src = sprite;
 
   pokemonTypes.innerText = "";
 
   types.forEach((el) => {
-    const type = document.createElement('span');
-    type.classList.add('type')
-    type.classList.add(el.type.name)
-    type.innerText = el.type.name.toUpperCase();
+    const type = document.createElement("span");
+    const typeName = el.type.name;
+    console.log(typeName);
+    type.classList.add("type");
+    type.classList.add(typeName);
+    type.style.backgroundColor = pokemonTypesColors[typeName];
+    type.innerText = typeName.toUpperCase();
     pokemonTypes.appendChild(type);
-  })
-
-}
+  });
+};
 
 const displayTable = (stats) => {
-  const statsObj = {}
+  const statsObj = {};
   stats.forEach((el) => {
-    console.log(el)
-    statsObj[el.stat.name] = el.base_stat
-  })
+    statsObj[el.stat.name] = el.base_stat;
+  });
 
-  const { hp, attack, defense, 'special-attack': specialAttack, 'special-defense': specialDefense, speed } = statsObj;
+  const {
+    hp,
+    attack,
+    defense,
+    "special-attack": specialAttack,
+    "special-defense": specialDefense,
+    speed,
+  } = statsObj;
 
   pokemonHp.innerText = hp;
   pokemonAttack.innerText = attack;
@@ -54,10 +81,9 @@ const displayTable = (stats) => {
   pokemonSpecialAttack.innerText = specialAttack;
   pokemonSpecialDefense.innerText = specialDefense;
   pokemonSpeed.innerText = speed;
+};
 
-}
-
-const testApi = async (pokemonName) => {
+const pokeApi = async (pokemonName) => {
   try {
     const response = await fetch(`${pokeUrl}${pokemonName.toLowerCase()}`);
     if (!response.ok) {
@@ -71,13 +97,11 @@ const testApi = async (pokemonName) => {
       id,
       stats,
       types,
-      sprites: { front_default: sprite }
+      sprites: { front_default: sprite },
     } = pokemon;
 
     displayScreen(name, id, weight, height, sprite, types);
     displayTable(stats);
-
-
   } catch (error) {
     alert("Pokemon not found");
   }
@@ -91,5 +115,5 @@ searchButton.addEventListener("click", (event) => {
     alert("Enter a Pokémon name");
     return;
   }
-  testApi(searchValue);
+  pokeApi(searchValue);
 });
